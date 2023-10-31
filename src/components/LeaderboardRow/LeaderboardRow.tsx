4bdd97945b73187876322b7c87ce4bbff372258b
Kaspar Marnat <kasparmarnat@gmail.com>
2023-10-31 15:10:14 +0200

    fixed menu sizing, started with menu

--- src/components/LeaderboardRow/LeaderboardRow.tsx
+++ src/components/LeaderboardRow/LeaderboardRow.tsx
@@ -12,7 +12,7 @@ const LeaderboardRow: React.FC<LeaderboardRowProps> = ({ position, image, user, 
       <div className="user">
         <img src={image} alt="" className="user__image" />
 
-        <h4 className="user__name">{user}</h4>
+        <h5 className="user__name">{user}</h5>
       </div>
       <p className="leaderboardrow__points">{score}</p>
     </div>
